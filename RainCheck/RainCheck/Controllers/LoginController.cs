a493522d089366b3fbf67a126c3d194c2b1c0fe4
AiLingSong <nju.ailingsong@gmail.com>
2016-12-05 16:28:11 -0500

    login controller,  login views

---
+++ RainCheck/RainCheck/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RainCheck.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+        public ActionResult CheckLogin()
+        {
+            return View("LoginPage");
+        }
+
+
+        public ActionResult ForgetUsername() { return View(); }
+        public ActionResult SendEmailResetPassword() { return View(); }
+
+        public ActionResult ForgetPassword() { return View(); }
+        public ActionResult ResetPassword() { return View(); }
+    }
+}
