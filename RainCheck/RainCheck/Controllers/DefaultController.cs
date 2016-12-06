799e291c267db5826f384a1162fc5770077c3294
NanHeSam <samhenan@gmail.com>
2016-12-06 10:58:50 -0500

    edit layout page

--- RainCheck/RainCheck/Controllers/DefaultController.cs
+++ RainCheck/RainCheck/Controllers/DefaultController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using RainCheck.Models;
 
 namespace RainCheck.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
-        public ActionResult Index()
+        public ActionResult Index() 
         {
-            return View();
+            Models.RainCheckConnectionString obj = new Models.RainCheckConnectionString();
+            List<state> stateObject = obj.states.ToList<state>();
+            return View(stateObject);
         }
     }
 }
