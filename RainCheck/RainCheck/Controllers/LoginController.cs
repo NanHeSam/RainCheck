2d2cc0182b5f13e052f76a7a72a257a8c613d8ba
AiLingSong <nju.ailingsong@gmail.com>
2016-12-06 14:15:19 -0500

    added views for login and home controller

--- RainCheck/RainCheck/Controllers/LoginController.cs
+++ RainCheck/RainCheck/Controllers/LoginController.cs
@@ -13,16 +13,56 @@ namespace RainCheck.Controllers
         {
             return View();
         }
-        public ActionResult CheckLogin()
+
+        public ActionResult LoginSubmit()
         {
+            var username = Request.Form["Username"];
+            var password = Request.Form["Password"];
             return View("LoginPage");
         }
 
 
-        public ActionResult ForgetUsername() { return View(); }
-        public ActionResult SendEmailResetPassword() { return View(); }
+        public ActionResult ForgetUsername()
+        {
+            return View();
+        }
 
-        public ActionResult ForgetPassword() { return View(); }
-        public ActionResult ResetPassword() { return View(); }
+        public ActionResult DisplayUsername()
+        {
+            var email = Request.Form["Email"];
+            var policyNum = Request.Form["Policy"];
+            // if inputed information is incorrect
+            // return View("ForgetUsername");
+
+            // else
+            return View();
+        }        
+
+        public ActionResult ForgetPassword()
+        {
+            return View();
+        }
+        public ActionResult SendEmailResetPassword()
+        {
+            var username = Request.Form["Username"];
+            var policyNum = Request.Form["Policy"];
+            // if inputed information is incorrect
+            // return View("ForgetPassword");
+
+            // else
+            // email verification code to user email address and then
+            // display reset password form, with verification code.
+            return View("EnterVerifyCode");
+        }
+        public ActionResult verifyCode() {
+            if (Convert.ToInt32(Request.Form["VerificationCode"]) == 0)
+                return View("EnterVerifyCode");
+            else
+                return View("ResetPassword");
+        }
+        public ActionResult ResetPassword()
+        {
+            return View("ResetPasswordResult");
+        }
     }
 }
