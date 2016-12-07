1a6769f5919463860b3de56247cb25d79f3a1e5b
vivek1menon <peacevan27@gmail.com>
2016-12-07 10:47:01 -0500

    added jquery folder and a view

---
+++ RainCheck/RainCheck/Controllers/GetQuoteController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RainCheck.Models;
+
+namespace RainCheck.Controllers
+{
+    public class GetQuoteController : Controller
+    {
+        // GET: GetQuote
+        public ActionResult Get()
+        {
+            return View("GetQuote");
+        }
+        public ActionResult Submit()
+        {
+            GetQuoteBLL qobj = new GetQuoteBLL();
+            qobj.FirstName = Request.Form["firstname"];
+            qobj.MiddleName = Request.Form["middlename"];
+            qobj.LastName = Request.Form["lastname"];
+            qobj.Address = Request.Form["streetaddress" + "apt"];
+            qobj.City = Request.Form["city"];
+            qobj.State = Request.Form["state"];
+            qobj.Zip = Int32.Parse(Request.Form["zip"]);
+            qobj.DOB = Request.Form["dob"];
+            qobj.Year = Int32.Parse(Request.Form["Year"]);
+            qobj.Make = Request.Form["Make"];
+            qobj.Model = Request.Form["Model"];
+            qobj.CarRelation = Request.Form["vehiclerel"];
+            qobj.Usage = Request.Form["usage"];
+            qobj.Milage = Int32.Parse(Request.Form["mileage"]);
+            try
+            {
+                qobj.Marital = Convert.ToBoolean(Request.Form["marital"]);
+                qobj.Military = Convert.ToBoolean(Request.Form["military"]);
+                qobj.Veteran = Convert.ToBoolean(Request.Form["veteran"]);
+                qobj.Accidents = Convert.ToBoolean(Request.Form["accidentradio"]);
+                qobj.Tickets = Convert.ToBoolean(Request.Form["ticketradio"]);
+                qobj.DUIs = Convert.ToBoolean(Request.Form["duiradio"]);
+                qobj.Suspension = Convert.ToBoolean(Request.Form["suspensionradio"]);
+                qobj.DefensiveDriver = Convert.ToBoolean(Request.Form["defensiveradio"]);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Dont know why this happens!!!");
+            }
+            qobj.Gender = Request.Form["optradio"];
+            qobj.Education = Request.Form["education"];
+            qobj.Employment = Request.Form["employment"];
+            qobj.Email = Request.Form["email"];
+            qobj.PhoneNum = long.Parse(Request.Form["phonenumber"]);
+            return View("QuoteView", qobj);
+        }
+    }
+}
