4b26f389507267f3183bc7f270178806859d09fc
NanHeSam <samhenan@gmail.com>
2016-12-09 15:28:28 -0500

    layout + usercontrol

--- RainCheck/RainCheck/Controllers/UserAccountController.cs
+++ RainCheck/RainCheck/Controllers/UserAccountController.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RainCheck.Models;
+using System.Data.Entity;
 
 namespace RainCheck.Controllers
 {
     public class UserAccountController : Controller
     {
+        private RainCheckConnectionString _context;
+        public UserAccountController()
+        {
+            _context = new RainCheckConnectionString();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: UserAccount
         public ActionResult UserMain()
         {
-            Models.RainCheckConnectionString obj = new Models.RainCheckConnectionString();
-            return View();
+            Session["logged_username"] = "Sam";
+            var policies = _context.policy_tbl.ToList();
+            return View(policies);
 
         }
     }
