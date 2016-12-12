3525fbcdbdd9ff9308dc95272c722d4a7c050809
hocine82 <hamidoucheh@gmail.com>
2016-12-12 18:12:57 -0500

    kkkh

---
+++ RainCheck/RainCheck/Controllers/UserController.cs
@@ -0,0 +1,288 @@
+﻿using RainCheck.common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RainCheck.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        
+        public ActionResult Load()
+        {
+            customer_tbl ct = new customer_tbl {customer_id = 1, userid = 2, driver_license_number = 22221982 , join_date = Convert.ToDateTime("12/12/1982")};
+            return View("LoadData",ct);
+        }
+        [HttpPost]
+        [MultipleButton(Name = "action", Argument = "StartPolicy")]
+        public ActionResult StartPolicy(customer_tbl ob)
+        {
+            //customer_tbl ct = new customer_tbl();
+            if (ModelState.IsValid)
+            {
+                policy_tbl pl = new policy_tbl { car_id = 1, start_date = Convert.ToDateTime("12/12/1982"), end_date = Convert.ToDateTime("12/12/1982"), opposite_body = 1, policy_amount = 200, policy_id = 2, self_body = 1, policy_number = 2443662, opposite_property = 2, self_property = 1, user_id = 1 };
+                TempData["DriverLicence"] = Request.Form["driver_license_number"];
+                return View("NewPolicy", pl);
+            }
+            else
+            {
+                return View("LoadData", ob);
+            }
+        }
+        [HttpPost]
+        [MultipleButton(Name = "action", Argument = "CreateAccount")]
+        public ActionResult CreateAccount(policy_tbl p)
+        {
+
+
+            bool validDate = false;
+            string s1 = Request.Form["start_date"].ToString();
+            string s2 = Request.Form["end_date"].ToString();
+            DateTime d1 = Convert.ToDateTime(s1);
+            DateTime d2 = Convert.ToDateTime(s2);
+            if (DateTime.Compare(d1, d2) == 0 || DateTime.Compare(d1, d2)>0)
+            {
+                validDate = false;
+                ViewBag.vlid = "The End Date must be greater than Start Date";
+            }
+            else
+            {
+                validDate = true;
+            }
+            if (ModelState.IsValid && validDate)
+            {
+                TempData["SD"] = Request.Form["start_date"];
+                TempData["ED"] = Request.Form["end_date"];
+                TempData["SP"] = Request.Form["SelfProperty"];
+                TempData["SB"] = Request.Form["SelfBody"];
+                TempData["OP"] = Request.Form["OppositeProperty"];
+                TempData["OB"] = Request.Form["OppositeBody"];                
+
+                login l = new login { login_id = 1, customer_id = 1, password = " ", user_name = " " };                
+                return View("NewAccount",l);
+            }
+            else
+            {
+                return View("NewPolicy", p);
+            }
+        }
+        public ActionResult Submit(login ls)
+        {
+            RainCheckServerEntities obContext = new RainCheckServerEntities();
+            string usn = Request.Form["user_name"].ToString();
+            string psw = Request.Form["password"].ToString();
+            string cpw = Request.Form["confirms"].ToString();
+            bool confirm = false;
+            bool samUser = false;
+            bool samPassword = false;
+           
+            List<login> lgs = obContext.logins.ToList();
+            ViewBag.UN = "";
+            ViewBag.PW = "";
+            ViewBag.CPW = "";
+            if(psw == cpw)
+            {
+                confirm = true;
+            }
+            else
+            {
+                ViewBag.CPW = "Your password does not match, please confim it"; 
+            }
+            foreach (login l in lgs)
+            {
+                if (String.Equals(l.user_name, usn))
+                {
+                    samUser = true;
+                    ViewBag.UN = "This Username has been taken, please choose another one";
+                }
+            }
+            foreach (login l in lgs)
+            {
+                if (String.Equals(l.password, psw))
+                {
+                    samPassword = true;
+                    ViewBag.PW = "This Password has been taken, please choose another one";
+                }
+            }
+
+            if (ModelState.IsValid && !samPassword && !samUser && confirm)
+            {
+                //Retrieve the quote object Through Seesion Here:[...........]
+                RainCheckServerEntities objContext = new RainCheckServerEntities();
+                List<quote> qs = objContext.quotes.ToList();
+                quote newQuote = new quote();
+                string dl = TempData["DriverLicence"].ToString();
+                TempData.Keep("DriverLicence");
+                foreach (quote q in qs)
+                {
+                    if (q.Reference_number == dl)
+                        newQuote = q;
+                }
+                decimal userID = newQuote.userid;
+                user_tbl newUser = new user_tbl();
+                List<user_tbl> ut = objContext.user_tbl.ToList();
+                foreach (user_tbl x in ut)
+                {
+                    if (x.userid == userID)
+                        newUser = x;
+                }                
+                //set up the amount on each coverage for the customer
+                decimal policy_amount = 0;
+                decimal p1 = getAmount(TempData.Peek("SP").ToString());
+                TempData.Keep("SP");
+                decimal p2 = getAmount(TempData.Peek("SB").ToString());
+                TempData.Keep("SB");
+                decimal p3 = getAmount(TempData["OP"].ToString());
+                TempData.Keep("OP");
+                decimal p4 = getAmount(TempData["OB"].ToString());
+                TempData.Keep("OB");
+
+                //set up the coverage level for the customer
+                decimal selfPro = getCoverage(TempData["SP"].ToString());
+                decimal selfBod = getCoverage(TempData["SB"].ToString());
+                decimal oppPro = getCoverage(TempData["OP"].ToString());
+                decimal oppBod = getCoverage(TempData["OB"].ToString());
+
+                Random rnd = new Random();
+                ViewBag.randm = rnd.Next(100000000, 1000000000);
+                string startDate = TempData.Peek("SD").ToString();
+                string endDate = TempData.Peek("ED").ToString();
+                string DL = dl;
+                ViewBag.st = startDate;
+                ViewBag.ed = endDate;
+                decimal total = policy_amount + newQuote.quote_amount + p1 + p2 + p3 + p4;
+                ViewBag.Amount = policy_amount + newQuote.quote_amount + p1+ p2 + p3 + p4;
+                //Create a new customer in the database  *******************************************/      
+                customer_tbl ct = new customer_tbl();
+                ct.userid = userID;
+                ct.join_date = Convert.ToDateTime(startDate);            
+                ct.driver_license_number = Decimal.Parse(DL);
+               
+                objContext.customer_tbl.Add(ct);
+                //objContext.SaveChanges();
+                decimal custid = 0;
+                List<customer_tbl> customerTable = objContext.customer_tbl.ToList();
+                foreach (customer_tbl cs in customerTable)
+                {
+                    if (cs.customer_id > custid)
+                    {
+                        custid = cs.customer_id;
+                    }
+                }
+                //Create a user Login ****************************************************************************/
+                string un = Request.Form["user_name"].ToString();
+                string pw = Request.Form["password"].ToString();
+                bool sameUser = false;
+                bool samePassword = false;
+                List<login> lns = objContext.logins.ToList();
+                //login ln = lns.ElementAt<login>(0);
+                login lg = new login();
+                foreach (login l in lns)
+                {
+                    if (String.Equals(l.user_name, un))
+                    {
+                        sameUser = true;
+                    }
+                }
+                foreach (login l in lns)
+                {
+                    if (String.Equals(l.password, pw))
+                    {
+                        samePassword = true;
+                    }
+                }
+                lg.customer_id = custid;
+                lg.user_name = un;
+                lg.password = pw;
+                if (!(sameUser) && !(samePassword))
+                {
+                    objContext.logins.Add(lg);
+                    // objContext.SaveChanges();
+                }
+                //Create a new Policy with a new coverage *********************************************************************************/
+
+                //Retrieve the car associted with this policy[getting the car object through session]
+                /*List<car> crs = objContext.cars.ToList();               
+                decimal vin = 5;
+                decimal carid = 0;
+                foreach (car x in crs)
+                {
+                    if (x.vin_number == vin)
+                        carid = x.car_id;
+                }*/
+                //set up the policy for a car
+                Random rd = new Random();
+                policy_tbl policy = new policy_tbl();
+                policy.policy_number = rnd.Next(100000000, 1000000000);
+                policy.car_id = 6; policy.user_id = userID; policy.start_date = Convert.ToDateTime(startDate);
+                policy.end_date = Convert.ToDateTime(endDate); policy.policy_amount = total; policy.self_property = selfPro;
+                policy.self_body = selfBod; policy.opposite_property = oppPro; policy.opposite_body = oppBod;
+                objContext.policy_tbl.Add(policy);
+                // objContext.SaveChanges();            
+
+                 return View("WelcomePage");
+            }
+            else
+            {
+                return View("NewAccount",ls);
+            }
+        }
+        //Get the coverage Level
+        public decimal getCoverage(string coverage)
+        {
+            int cov = 1;
+            if (coverage == "0")
+            {
+                cov = 1;
+            }
+            else if (coverage == "10000")
+            {
+                cov = 2;
+            }
+            else if (coverage == "25000")
+            {
+                cov = 3;
+            }
+            else if (coverage == "50000")
+            {
+                cov = 4;
+            }
+            else if (coverage == "100000")
+            {
+                cov = 5;
+            }
+            return cov;
+        }
+
+        //Get the Sucharge Amount for given policy
+        public decimal getAmount(string amt)
+        {
+            decimal amnt = 0;
+            if (amt == "0")
+            {
+                amnt = 0;
+            }
+            else if (amt == "10000")
+            {
+                amnt = 10;
+            }
+            else if (amt == "25000")
+            {
+                amnt = 25;
+            }
+            else if (amt == "50000")
+            {
+                amnt = 50;
+            }
+            else if (amt == "100000")
+            {
+                amnt = 100;
+            }
+
+            return amnt;
+        }
+    }
+}
