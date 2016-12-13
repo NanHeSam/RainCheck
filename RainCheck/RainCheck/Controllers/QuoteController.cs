a5dfc2d0ca273cc4a46b73ce4737d0073238ff83
AiLingSong <nju.ailingsong@gmail.com>
2016-12-13 11:26:34 -0500

    deleted git ignore

---
+++ RainCheck/RainCheck/Controllers/QuoteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RainCheck.Controllers
+{
+    public class QuoteController : Controller
+    {
+        // GET: Quote
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetQuote()
+        {
+            return View();
+        }
+
+        public ActionResult QuoteResult()
+        {
+            return View();
+        }
+    }
+}
