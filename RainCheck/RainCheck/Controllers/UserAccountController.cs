22bc538add6489515976b6465f228c8ac4bb66ab
NanHeSam <samhenan@gmail.com>
2016-12-13 00:39:53 -0500

    change policy view

--- RainCheck/RainCheck/Controllers/UserAccountController.cs
+++ RainCheck/RainCheck/Controllers/UserAccountController.cs
@@ -25,9 +25,32 @@ namespace RainCheck.Controllers
         public ActionResult UserMain()
         {
             Session["logged_username"] = "Sam";
-            var policies = _context.policy_tbl.ToList();
+            var policies = new policyVM(1);
             return View(policies);
 
         }
+        
+        public ActionResult submit()
+        {
+            List<coverage_level> coverage = _context.coverage_level.ToList<coverage_level>();
+            return View("ChangePolicy", coverage);
+        }
+
+        [HttpPost]
+        public ActionResult saveQuote(string save, string cancel)
+        {
+            if (!string.IsNullOrEmpty(save))
+            {
+                //Request.Form[""];
+                return RedirectToAction("UserMain");
+            } else if (!string.IsNullOrEmpty(cancel))
+            {
+                return View();
+            }
+            return View();
+
+        }
+
+
     }
 }
--- RainCheck/RainCheck/Models/policyVM.cs
+++ RainCheck/RainCheck/Models/policyVM.cs
@@ -2,19 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace RainCheck.Models
 {
     public class policyVM
     {
-        public IEnumerable<policy_tbl> policies { get; set; }
-        public user_tbl user { get; set; }
-        public string userName
+        private RainCheckConnectionString _context = new RainCheckConnectionString();
+        decimal user_id;
+        public policyVM(decimal user_id)
+        {
+            this.user_id = user_id;
+        }
+        public IEnumerable<policy_tbl> current_policy
         {
             get
             {
-                return user.user_F_name + " " + user.user_M_name;
+                return _context.policy_tbl
+                    .Where(o => o.user_id == user_id)
+                    .GroupBy(o => o.policy_number)
+                    .Select(ig => ig.OrderByDescending(t => t.start_date).FirstOrDefault()).ToList();
             }
         }
+        public IEnumerable<policy_tbl> policy_history
+        {
+            get
+            {
+                return _context.policy_tbl
+                    .Where(o => o.user_id == user_id);
+            }
+        }
+
+        public IQueryable<decimal> BodyCoverageLevel
+        {
+            get
+            {
+                var coverage = from a in _context.policy_tbl
+                               where a.user_id == user_id
+                               select  a.coverage_level.limite;
+                return coverage;
+
+            }
+        }
+
+        public IQueryable<decimal> PropertyDamage
+        {
+            get
+            {
+                var coverage = from a in _context.policy_tbl
+                               select a.coverage_level1.limite;
+                return coverage;
+
+            }
+        }
+
+
     }
 }
